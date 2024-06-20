2692f70f35a249a1b5ee3472939570dc5a6d0ba9
Doug Hirlinger <doughirlinger@gmail.com>
2024-06-19 21:32:43 -0400

    geez

--- arrays/exercises/part-five-arrays.js
+++ arrays/exercises/part-five-arrays.js
@@ -3,9 +3,27 @@ let arr = ['B', 'n', 'n', 5];
 
 //1) Use the split method on the string to identify the purpose of the parameter inside the ().
 
+console.log(str.split()); //it seems nothing changed.
+console.log(str.split('e')); //'e' was removed and also servered as the point at which a new array item was created. 
+console.log(str.split(' ')); // single white spaces were removed and were the points at which new array items were created.
+console.log(str.split('')); // each character (including white space) became a new array item. 
+console.log('============='); // the purpose of the parameter seems to the point of which an array itme is created and that point is also emitted. 
+
 //2) Use the join method on the array to identify the purpose of the parameter inside the ().
 
-//3) Do split or join change the original string/array?
+console.log(arr.join()); //a string is created with a coma between each array item. 
+console.log(arr.join('a'));// a string is created with 'a' between each array item. 
+console.log(arr.join(' ')); // a string is created with ' ' between each array item.
+console.log(arr.join('')); // a string is created with nothing between each array item.
+console.log('============='); //it seems the purpoes of the parameter is to choose a character (or not) to be placed between each character that was formally an array item. 
+
+//3) Do split or join change the original string/array? No. 
+
+console.log(str);
+console.log(arr);
+console.log('=============');
 
 //4) We can take a comma-separated string and convert it into a modifiable array. Try it! Alphabetize the cargoHold string, and then combine the contents into a new string.
 let cargoHold = "water,space suits,food,plasma sword,batteries";
+let alphabetizedString = cargoHold.split(',').sort();
+console.log(alphabetizedString);
--- arrays/exercises/part-four-arrays.js
+++ arrays/exercises/part-four-arrays.js
@@ -5,6 +5,23 @@ let holdCabinet2 = ['orange drink', 'nerf toys', 'camera', 42, 'parsnip'];
 
 //1) Print the result of using concat on the two arrays. Does concat alter the original arrays? Verify this by printing holdCabinet1 after using the method.
 
-//2) Print a slice of two elements from each array. Does slice alter the original arrays?
+console.log(holdCabinet1.concat(holdCabinet2));
+//the original arrays are uneffected.
+console.log(holdCabinet1);
+console.log(holdCabinet2);
+console.log('==========');
+//2) Print a slice of two elements from each array. Does slice alter the original arrays? 
+//No.
 
+console.log(holdCabinet1.slice(2,5));
+console.log(holdCabinet2.slice(1,4));
+console.log(holdCabinet1);
+console.log(holdCabinet2);
+console.log('==========');
 //3) reverse the first array, and sort the second. What is the difference between these two methods? Do the methods alter the original arrays?
+// the difference reverse simply reverses the order while sort converts each to a string and then orders them to match a certain set of rules that aren't necessarily intuitive.
+//reverse() and sort() do alter the arrays.  
+console.log(holdCabinet1.reverse());
+console.log(holdCabinet2.sort());
+console.log(holdCabinet1);
+console.log(holdCabinet2);
