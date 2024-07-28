9693dd3439c0351aed003f567df2f1681a5140f8
Doug Hirlinger <doughirlinger@gmail.com>
2024-07-27 22:11:04 -0400

    complete

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,20 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
-    
-}
+    button.addEventListener("click", event => {
+        paragraph.innerHTML = "Houston, we have liftoff!"
+    });
+    missionAbort.addEventListener("mouseover", event => {
+        missionAbort.style.backgroundColor = "red";
+    });
+    missionAbort.addEventListener("mouseout", event => {
+        missionAbort.style.backgroundColor = "";
+    });
+    missionAbort.addEventListener("click", (event) => {
+        if (confirm("Are you sure you want to abort the mission?")) {
+            paragraph.innerHTML = "Mission aborted! Space shuttle returning home.";
+        }
+    });
+} 
 
 window.addEventListener("load", init);
