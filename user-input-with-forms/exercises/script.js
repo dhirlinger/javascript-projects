2f17af0da4b13d92eb984733949357ea8f123ced
Doug Hirlinger <doughirlinger@gmail.com>
2024-08-01 19:42:22 -0400

    complete

--- user-input-with-forms/exercises/script.js
+++ user-input-with-forms/exercises/script.js
@@ -1,1 +1,18 @@
 //Code Your Solution Below
+window.addEventListener("load", function(){
+    let form = document.querySelector("form");
+    form.addEventListener("submit", function(event){
+        let testNameInput = document.querySelector("input[name=testName]");
+        let testDateInput = document.querySelector("input[name=testDate]");
+        let rocketTypeInput = document.querySelector("select[name=rocketType]");
+        let boosterCountInput = document.querySelector("input[name=boosterCount]");
+        let windRatingInput = document.querySelector("input[name=windRating]");
+        let productionServersInput = document.querySelector("input[name=productionServers]");
+        if (testNameInput.value === '' || testDateInput.value === '' || rocketTypeInput.value === '' || boosterCountInput.value === ''
+            || windRatingInput.value === '' || productionServersInput.value === '') {
+                alert("All fields are required!");
+                // stop the form submission
+                event.preventDefault();
+            }
+    });
+});
